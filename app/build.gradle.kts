ec14d381d52238cd82efcdad9469d2bf250e8e25
SahilS <ssaini@appinfoinc.com>
2024-12-16 14:12:11 +0530

    issue..

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.customtoast"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.customtoast"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
